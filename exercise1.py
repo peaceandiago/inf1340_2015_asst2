abc00c117187fc2e241ece3f0501af90ba21312d
peaceandiago <paola.santiago@mail.utoronto.ca>
2015-10-22 11:26:36 -0400

    Start for If

--- exercise1.py
+++ exercise1.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+`#!/usr/bin/env python
 
 """ Assignment 2, Exercise 1, INF1340, Fall, 2015. Pig Latin
 
@@ -10,17 +10,20 @@ __author__ = 'Susan Sim'
 __email__ = "ses@drsusansim.org"
 __copyright__ = "2015 Susan Sim"
 __license__ = "MIT License"
+#"""
+ #   Describe your function
 
+  #  :param :
+   # :return:
+    #:raises:
+
+   # """
+   # result = ""
 
 def pig_latinify(word):
-    """
-    Describe your function
-
-    :param :
-    :return:
-    :raises:
-
-    """
-    result = ""
+    #begins with vowel add yay to end of word
+    vowel = ["a", "e", "i", "o", "u"]
+    word = raw_input("Let's make it piglatinfied")
+    if word
 
     return result
