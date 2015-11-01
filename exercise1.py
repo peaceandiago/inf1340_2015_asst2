23b95af743a45dcb81e90fa3510d181445950e54
Bertha <bertha.chan@mail.utoronto.ca>
2015-11-01 14:12:38 -0500

    ex. 1 updated comments

--- exercise1.py
+++ exercise1.py
@@ -13,33 +13,36 @@ __license__ = "MIT License"
 #"""
  #   Describe your function
 
-  #  :param :
+  #  :param : word passed on to the function
    # :return:
-    #:raises:
+    #:raises: if an integer is entered, raises AttributeError
 
    # """
    # result = ""
 
 
-def pig_latinify(word):
+def pig_latinify(child_talk):
 
     # convert string to lowercase
-    childtalk = word.lower()
+    child_talk = child_talk.lower()
 
     # begins with vowel add yay to end of word
     vowel = ["a", "e", "i", "o", "u"]
-    first_letter = childtalk[0]
+    first_letter = child_talk[0]
     if first_letter in vowel:
-        print (childtalk + "yay")
+        print (child_talk + "yay")
 
     # begins with consonant, remove all consonants from beginning, append in end + ay
-    if first_letter not in vowel:
-        for i in range(len(childtalk)):
-            if childtalk[i] in vowel:
+    elif first_letter not in vowel:
+        for i in range(len(child_talk)):
+            # Assigning letter_vowel as i
+            if child_talk[i] in vowel:
                 letter_vowel = i
                 break
+        print(child_talk[letter_vowel:] + child_talk[:letter_vowel] + "ay")
 
-        print(childtalk[letter_vowel:] + childtalk[:letter_vowel] + "ay")
+    else:
+        raise AttributeError
 
 
-pig_latinify("scratch")
+pig_latinify("spoon")
