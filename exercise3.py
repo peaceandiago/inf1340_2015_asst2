362ff332635335128fc583d7523d9f56fb7b1fc2
Bertha <bertha.chan@mail.utoronto.ca>
2015-11-02 13:22:43 -0500

    intersection function done

--- exercise3.py
+++ exercise3.py
@@ -12,26 +12,50 @@ __email__ = "ses@drsusansim.org"
 __copyright__ = "2015 Susan Sim"
 __license__ = "MIT License"
 
+GRADUATES = [["Number", "Surname", "Age"],
+             [7274, "Robinson", 37],
+             [7432, "O'Malley", 39],
+             [9824, "Darkes", 38]]
 
-def union(table1, table2):
-    """
-    Perform the union set operation on tables, table1 and table2.
+MANAGERS = [["Number", "Surname", "Age"],
+            [9297, "O'Malley", 56],
+            [7432, "O'Malley", 39],
+            [9824, "Darkes", 38]]
 
-    :param table1: a table (a List of Lists)
-    :param table2: a table (a List of Lists)
-    :return: the resulting table
-    :raises: MismatchedAttributesException:
-        if tables t1 and t2 don't have the same attributes
-    """
-    return []
-
+# def union(table1, table2):
+#     """
+#     Perform the union set operation on tables, table1 and table2.
+#
+#     :param table1: a table (a List of Lists)
+#     :param table2: a table (a List of Lists)
+#     :return: the resulting table
+#     :raises: MismatchedAttributesException:
+#         if tables t1 and t2 don't have the same attributes
+#     """
+#
+#     for rows1 in table1:
+#         for rows2 in table2:
+#             if rows1 == rows2:
+#                 print(rows1,"vs", rows2)
+#
+#     return []
+#
+# union(GRADUATES, MANAGERS)
 
 def intersection(table1, table2):
     """
     Describe your function
 
     """
-    return []
+    intersection_list = []
+    for rows1 in table1:
+        for rows2 in table2:
+            if rows1 == rows2:
+                intersection_list.append(rows1)
+
+    return intersection_list
+
+print(intersection(MANAGERS, GRADUATES))
 
 
 def difference(table1, table2):
