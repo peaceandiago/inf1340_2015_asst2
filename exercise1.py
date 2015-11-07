bc83f13cb8cc20fff115642c9608d9a0bac890d4
Bertha <bertha.chan@mail.utoronto.ca>
2015-11-06 22:46:17 -0500

    Edited Comments

--- exercise1.py
+++ exercise1.py
@@ -14,42 +14,42 @@ __license__ = "MIT License"
 # Describe your function
 # :param : word passed on to the function
 # :return: if begins with vowel, add "yay" to the end.
-# if begins with consonant, move consonant(s) to end and add "ay"
+# If it begins with consonant, move consonant(s) to end and add "ay"
 # :raises: if an integer is entered, raises AttributeError
 
 vowel = ["a", "e", "i", "o", "u"]
 
 
 def pig_latinify(word):
-    # convert input to lowercase
+    # Convert input to lowercase
     word = word.lower()
 
-    # convert input to string
+    # Convert input to string
     word = str(word)
 
     letter_vowel_index = -1
 
-    # searches for vowel
+    # Searches for vowel
     for i in range(len(word)):
         # Assigning letter_vowel as i
         if word[i] in vowel:
             letter_vowel_index = i
             break
 
-    # if word begins with vowel, add yay to end of word
+    # If word begins with vowel, add yay to end of word
     first_letter = word[0]
     if first_letter in vowel:
         return word + "yay"
 
-    # begins with consonant, remove all consonants from beginning, append in end + ay
+    # Begins with consonant, remove all consonants from beginning, append in end + ay
     elif first_letter not in vowel and letter_vowel_index == -1:
         return word + "ay"
 
-    # if word does not have vowels, return original word + ay
+    # If word does not have vowels, return original word + ay
     elif first_letter not in vowel and letter_vowel_index != -1 and letter_vowel_index != 0:
         return word[letter_vowel_index:] + word[:letter_vowel_index] + "ay"
 
-    # if entering a non-string, raise AttributeError
+    # If entering a non-string, raise AttributeError
     else:
         raise AttributeError
 
