bb148f69ea1904c51ef9d318afca7a8dffe8467a
Bertha <bertha.chan@mail.utoronto.ca>
2015-11-06 22:42:35 -0500

    Added Return None

--- exercise3.py
+++ exercise3.py
@@ -79,39 +79,49 @@ def intersection(table1, table2):
          if tables t1 and t2 don't have the same attributes
 
     """
+    # If the two tables do not have matching schema, raise Exception
     if table1[0] != table2[0]:
         raise MismatchedAttributesException
     intersection_list = []
     for rows1 in table1:
-        for rows2 in table2:
-            if rows1 == rows2:
+        if rows1 in table2:
                 intersection_list.append(rows1)
-    intersection_list = remove_duplicates(intersection_list)
 
-    return intersection_list
+    # If the two tables only have matching headers, return none
+    if len(intersection_list) > 1:
+        return intersection_list
+    else:
+        return None
 
 
 def difference(table1, table2):
     """
      Perform the difference set operation on tables, table1 and table2.
-
      :param table1: a table (a List of Lists)
      :param table2: a table (a List of Lists)
      :return: the resulting table that contains all unique rows that appear in table1 only, not table2
      :raises: MismatchedAttributesException:
          if tables t1 and t2 don't have the same attributes
-
     """
+    # If the two tables do not have matching schema, raise Exception
     if table1[0] != table2[0]:
         raise MismatchedAttributesException
-    # find the intersected rows from table1 and table2
+
+    # Find the intersected rows from table1 and table2
     intersected_list = intersection(table1, table2)
-    # delete the first row of intersected list to avoid delete the attribute [0]
+
+    # If the two tables only have matching header, return none
+    if intersected_list is None:
+        return None
+
+    # Delete the first row of intersected list to avoid delete the attribute [0]
     del intersected_list[0]
     for intersected_row in intersected_list:
         for table1_row in table1:
             if table1_row == intersected_row:
-                # remove duplicated attributes
+                # Remove duplicated attributes
                 table1.remove(table1_row)
     table1 = remove_duplicates(table1)
     return table1
+
+# print(difference(GRADUATES, MANAGERS))
